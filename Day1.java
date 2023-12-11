254ade6c2b1de3d87040d9ef0e59ebbd3ba1eda4
Komron <komron.ba@gmail.com>
2023-12-11 11:11:37 +0100

    Added Day1.java

---
+++ Day1.java
@@ -0,0 +1,15 @@
+package AdventOfCode2023;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Day1 {
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String line = bufferedReader.readLine();
+        
+    }
+
+}
